727b4d805711e6db94a69ce16f76f55fbe91ceb0
Fayaz K <info@fayazk.com>
2024-07-27 15:56:39 +0500

    ♻️ (Welcome.jsx): refactor layout structure to simplify the component and enhance readability ✨ (Welcome.jsx): replace complex layout with a simple Flex component and add a welcome title for better user experience

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
-import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import {Breadcrumb, Flex, Layout, Menu, theme, Typography} from 'antd';
 const { Header, Content, Footer, Sider } = Layout;
 const items1 = ['1', '2', '3'].map((key) => ({
     key,
@@ -26,80 +26,9 @@ const Welcome = () => {
         token: { colorBgContainer, borderRadiusLG },
     } = theme.useToken();
     return (
-        <Layout>
-            <Header
-                style={{
-                    display: 'flex',
-                    alignItems: 'center',
-                }}
-            >
-                <div className="demo-logo" />
-                <Menu
-                    theme="dark"
-                    mode="horizontal"
-                    defaultSelectedKeys={['2']}
-                    items={items1}
-                    style={{
-                        flex: 1,
-                        minWidth: 0,
-                    }}
-                />
-            </Header>
-            <Content
-                style={{
-                    padding: '0 48px',
-                }}
-            >
-                <Breadcrumb
-                    style={{
-                        margin: '16px 0',
-                    }}
-                >
-                    <Breadcrumb.Item>Home</Breadcrumb.Item>
-                    <Breadcrumb.Item>List</Breadcrumb.Item>
-                    <Breadcrumb.Item>App</Breadcrumb.Item>
-                </Breadcrumb>
-                <Layout
-                    style={{
-                        padding: '24px 0',
-                        background: colorBgContainer,
-                        borderRadius: borderRadiusLG,
-                    }}
-                >
-                    <Sider
-                        style={{
-                            background: colorBgContainer,
-                        }}
-                        width={200}
-                    >
-                        <Menu
-                            mode="inline"
-                            defaultSelectedKeys={['1']}
-                            defaultOpenKeys={['sub1']}
-                            style={{
-                                height: '100%',
-                            }}
-                            items={items2}
-                        />
-                    </Sider>
-                    <Content
-                        style={{
-                            padding: '0 24px',
-                            minHeight: 280,
-                        }}
-                    >
-                        Content
-                    </Content>
-                </Layout>
-            </Content>
-            <Footer
-                style={{
-                    textAlign: 'center',
-                }}
-            >
-                Ant Design ©{new Date().getFullYear()} Created by Ant UED
-            </Footer>
-        </Layout>
+        <Flex justify={'space-around'} align={'center'} style={{ height: '100vh' }}>
+            <Typography.Title>Welcome to Liar</Typography.Title>
+        </Flex>
     );
 };
 export default Welcome;
