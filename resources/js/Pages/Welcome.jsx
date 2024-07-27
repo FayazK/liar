572515a2d0bb4cf2c3aef5efd095bc247288fd4c
Fayaz K <info@fayazk.com>
2024-07-27 16:00:12 +0500

    ♻️ (Welcome.jsx): refactor Welcome component by removing unused imports and code to enhance readability and maintainability

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,34 +1,11 @@
-import React from 'react';
-import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
-import {Breadcrumb, Flex, Layout, Menu, theme, Typography} from 'antd';
-const { Header, Content, Footer, Sider } = Layout;
-const items1 = ['1', '2', '3'].map((key) => ({
-    key,
-    label: `nav ${key}`,
-}));
-const items2 = [UserOutlined, LaptopOutlined, NotificationOutlined].map((icon, index) => {
-    const key = String(index + 1);
-    return {
-        key: `sub${key}`,
-        icon: React.createElement(icon),
-        label: `subnav ${key}`,
-        children: new Array(4).fill(null).map((_, j) => {
-            const subKey = index * 4 + j + 1;
-            return {
-                key: subKey,
-                label: `option${subKey}`,
-            };
-        }),
-    };
-});
+import {LaptopOutlined, NotificationOutlined, UserOutlined} from '@ant-design/icons';
+import {Flex, Typography} from 'antd';
+
+
 const Welcome = () => {
-    const {
-        token: { colorBgContainer, borderRadiusLG },
-    } = theme.useToken();
-    return (
-        <Flex justify={'space-around'} align={'center'} style={{ height: '100vh' }}>
-            <Typography.Title>Welcome to Liar</Typography.Title>
-        </Flex>
-    );
+
+    return (<Flex justify={'space-around'} align={'center'} style={{height: '100vh'}}>
+        <Typography.Title>Welcome to Liar</Typography.Title>
+    </Flex>);
 };
 export default Welcome;
